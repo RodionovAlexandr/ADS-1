ad5be343700d04beefedd66bd0208e29019a3d9a
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-02-27 22:26:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,10 +7,10 @@ bool checkPrime(uint64_t value) {
 uint64_t i, sqvalue;
   sqvalue = value*value;
   for (int i = 2; i <= sqvalue; i++) {
-   if (value % i == 0) 
-     return false; 
+  if (value % i == 0) 
+  return false; 
   }
-    return true; 
+  return true; 
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -19,12 +19,12 @@ uint64_t nPrime(uint64_t n) {
   count = 1;
   while (true) {
   if (n == count) {
-    return np;
-    }
+  return np;
+  }
   np++;
   if (checkPrime(np)) count++;
   }
-}
+  }
 
 uint64_t nextPrime(uint64_t value) {
 uint64_t np;
@@ -33,15 +33,16 @@ uint64_t np;
   np++;
   if (checkPrime(np)) return np;
   }
-}
+  }
 
 uint64_t sumPrime(uint64_t hbound) {
-if (hbound == 1 || hbound == 0) return 0;
+  if (hbound == 1 || hbound == 0) return 0;
   hbound--;
   if (checkPrime(hbound)) {
-    return (hbound + sumPrime(hbound));
-    }
-    else if (!checkPrime(hbound)) {
-    return sumPrime(hbound);
-    }
+  return (hbound + sumPrime(hbound));
+  }
+  else 
+  if (!checkPrime(hbound)) {
+  return sumPrime(hbound);
+  }
 }
