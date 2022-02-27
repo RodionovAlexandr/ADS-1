c319431c69c0f401e1537547e4a059145622bca9
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-02-27 23:19:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,7 @@
 
 
 bool checkPrime(uint64_t value) {
-if (value > 1)
-{
-for (int i = 2; i < value; i++)
-if (value % i == 0)
-return true;
-} else {
-return false;
-}
-}
-
-uint64_t nPrime(uint64_t n) {
+// вставьте код функции
 uint64_t count, pr;
 pr = 2;
 count = 1;
@@ -24,10 +14,22 @@ return pr;
 }
 pr++;
 if (!checkPrime(pr)) count++;
+}  
+}
+
+uint64_t nPrime(uint64_t n) {
+// вставьте код функции
+if (value == 1) return 2;
+if (value == 2) return 3;
+if (checkPrime(value) == 1) value += 1;
+for (int i = value; i <= 10000; i++)
+if (checkPrime(i)) {
+return i;
 }
 }
 
 uint64_t nextPrime(uint64_t value) {
+// вставьте код функции
 if (value == 1) return 2;
 if (value == 2) return 3;
 if (checkPrime(value) == 1) value += 1;
@@ -38,8 +40,9 @@ return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t allin= 0;
-for(uint64_t i=1; i < hbound; i++)
+// вставьте код функции
+  uint64_t allin = 0;
+for(uint64_t i = 1; i < hbound; i++)
 if (checkPrime(i) == 1) {
 allin = allin + i;
 }
