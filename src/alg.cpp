ee7b80c39df48c72c14f24089c24e02039b8c1a3
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-03-05 23:55:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,46 +5,49 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-if (value > 1) {
-for (int j = 2; j < value; j++) {
-if (value % j == 0)
-return false;
-}
-else
-return true;
-}
+for (int i = 2; i <= value / 2; i++) {
+        if (value % i == 0) {
+            return false;
+            
+        }
+        else return true;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-uint64_t count, pr;
-pr = 2;
-count = 1;
-while (true) {
-if (n == count) {
-return pr;
-}
-pr++;
-if (checkPrime(pr)) count++;
-}
+uint64_t value,num;
+    value = 0;
+    num = 0;
+    while (num < n) {
+        value ++;
+        if (checkPrime(value) == true) {
+            num++;
+        }
+    }
+    return value;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-if (value == 1) return 2;
-if (value == 2) return 3;
-if (checkPrime(value) == 1) value += 1;
-for (int i = value; i <= 10000; i++) {
-if (checkPrime(i)) return i;
-}
+int i = 0;
+    while (i < 1) {
+        value++;
+        if (checkPrime(value) == true) {
+            i += 1;
+            return value;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-  uint64_t allin = 0;
-for(uint64_t i = 1; i < hbound; i++)
-if (checkPrime(i) == 1) {
-allin = allin + i;
-}
-return allin;
+  int sum = 0;
+    while (hbound > 1){
+        hbound -= 1;
+        if (checkPrime(hbound) == true) {
+            sum += hbound;
+        }
+    }
+    return sum;
 }
